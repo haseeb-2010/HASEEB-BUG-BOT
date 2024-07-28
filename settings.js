a79cd1be9402350fe839075c4e001546a75e11d6
haseeb-2010 <a.haseeb200999@gmail.com>
2024-07-28 08:48:55 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,15 +3,15 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923192173398"
-global.ownername = "🐛 MR WASI "
+global.ownernomer = "923097124814"
+global.ownername = "🐛 Abdul Haseeb"
 global.ytname = "YT: Dont have"
 global.socialm = "GitHub: private"
 global.location = "Pakistan, Lahore"
 
-global.ownernumber = '923347748038'  //creator number
-global.ownername = ' MR WASI ' //owner name
-global.botname = 'WASI TECH BUG BOT ' //name of the bot
+global.ownernumber = '923097124814'  //creator number
+global.ownername = ' Abdul Haseeb' //owner name
+global.botname = 'HASEEB BUG BOT ' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
